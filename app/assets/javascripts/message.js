bbf504685ed34e81740ace32eb5b4a887210b0d6
narushima1210 <61218136+narushima1210@users.noreply.github.com>
2020-03-03 19:29:51 +0900

    メッセージが投稿されたら一番下までスクロールするように記述

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -54,6 +54,7 @@ $(function(){
     .done(function(data){
       var html =buildHTML(data);
       $('.body').append(html);
+      $('.body').animate({ scrollTop: $('.body')[0].scrollHeight});
       $('form')[0].reset();
     })
   });
