464d2e61a9956bf628ed3a0bcddf8442f11ee891
narushima1210 <61218136+narushima1210@users.noreply.github.com>
2020-03-04 09:02:33 +0900

    jsで使うhtmlを記述した

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,43 @@
+$(function(){
+  function appendUser(user) {
+      var html = `
+                  <div class="chat-group-user clearfix">
+                    <p class="chat-group-user__name">${user.name}</p>
+                    <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id} data-user-name="${user.name}">追加</div>
+                  </div>`
+      $("#user-search-result").append(html);
+  }
+  
+  function appendErrMsgToHTML(msg) {
+    var html = `<div class = "chat-group-user clearfix">
+                  <p class ="chat-group-user__name">${msg}</p>
+                </div>`
+                $("#user-search-result").append(html);
+  }
+
+  $("#user-search-field").on("keyup", function(e){
+    var input = $("#user-search-field").val()
+    $.ajax({
+      type:'GET',
+      url:'/users',
+      dataType:'json',
+      data: { keyword: input }
+    })
+    .done(function(users){
+      $("#user-search-result").empty();
+
+      if (users.length !==0){
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      } else if (input.length == 0) {
+        return false;
+      } else {
+        appendErrMsgToHTML("一致するユーザーが見つかりませんでした");
+      }
+    })
+    .fail(function() {
+      alert("ユーザー検索に失敗しました");
+    })
+  })
+})
